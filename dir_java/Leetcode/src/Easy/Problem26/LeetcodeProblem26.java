284f3aed146372919fa9495aaa8f84aa877c9d56
ssudharsan93 <sudharsan.ssv@gmail.com>
2024-09-25 21:58:26 -0700

    Problem 26 solution fixed. Now is the correct solution

--- dir_java/Leetcode/src/Easy/Problem26/LeetcodeProblem26.java
+++ dir_java/Leetcode/src/Easy/Problem26/LeetcodeProblem26.java
@@ -4,29 +4,26 @@ class Solution {
     public static int removeDuplicates(int[] nums) {
         int p1 = 0; 
         int p2 = p1 + 1;
-        int numUniqElems = 1;
-        int searchVal = nums[p1];
         while ( p2 < nums.length ){
-            if ( searchVal == nums[p2] ){
-                System.out.println(nums[p1]);
-                System.out.println(nums[p2]);
-                while( ( p2 < nums.length ) && ( searchVal == nums[p2] ) ){
-                   p2 = p2 + 1;
-                }
-                if ( p2 == nums.length ) { return numUniqElems; }
-                searchVal = nums[p2];
-                nums[p1 + 1] = searchVal;
+            if ( nums[p1] != nums[p2] ){
+                nums[p1 + 1] = nums[p2];
+                p1 = p1 + 1;
             }
-
-            numUniqElems = numUniqElems + 1;
-            p1 = p1 + 1;
             p2 = p2 + 1;
         }
 
-        return numUniqElems;
+        return p1 + 1;
     }
 
     public static void main(String[] args){
-        removeDuplicates(new int[]{1, 2, 2});
+        int [] nums = new int[]{1,1,2,3};
+        int k = removeDuplicates(nums);
+
+        System.out.print("[ ");
+        for ( int i = 0; i < k; i++ ){
+            System.out.print(nums[i] + " ");
+        }
+        System.out.println("]");
+
     }
 }
