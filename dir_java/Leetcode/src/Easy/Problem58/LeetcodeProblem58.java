08ca2f8544e4c70f191af249102869ebc6a0aad5
ssudharsan93 <sudharsan.ssv@gmail.com>
2024-09-26 19:03:17 -0700

    Added package import for Problem 58 file. Correct solution for Leetcode Problem 65 (  Plus One )

--- dir_java/Leetcode/src/Easy/Problem58/LeetcodeProblem58.java
+++ dir_java/Leetcode/src/Easy/Problem58/LeetcodeProblem58.java
@@ -1,3 +1,6 @@
+package src.Easy.Problem58;
+
+//Length of Last Word
 class Solution {
     public int lengthOfLastWord(String s) {
         String[] words = s.split("\s");
---
+++ dir_java/Leetcode/src/Easy/Problem65/LeetcodeProblem65.java
@@ -0,0 +1,46 @@
+package src.Easy.Problem65;
+
+import java.lang.*;
+import java.util.*;
+
+// Plus One
+class Solution {
+    public static int[] plusOne(int[] digits) {
+        int carry = 1;
+        int nextCarry = 0;
+        ArrayList<Integer> digitsList = new ArrayList<>();
+        for ( int digIdx = digits.length - 1; digIdx >= 0; digIdx-- ){
+            if ( ( digits[digIdx] == 9 ) && ( carry == 1 ) ){
+                digitsList.add(0, Integer.valueOf(0));
+                nextCarry = 1;
+                if ( digIdx == 0 ){ digitsList.add(0, Integer.valueOf(1)); }
+            } else {
+                digitsList.add(0, digits[digIdx] + carry);
+                nextCarry = 0;
+            }
+
+            carry = nextCarry;
+        }
+
+        int[] res = new int[digitsList.size()];
+
+        for ( int resIdx=0; resIdx < digitsList.size(); resIdx++ ){
+            res[resIdx] = digitsList.get(resIdx).intValue();
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args){
+        //int[] result = plusOne(new int[]{9, 9, 9});
+        int[] result = plusOne(new int[]{9, 8, 9});
+        
+        System.out.print("[ ");
+        for ( int resIdx=0; resIdx < result.length; resIdx++ ){
+            System.out.print(result[resIdx]);
+            System.out.print(" ");
+        }
+
+        System.out.print(" ]");
+    }
+}
