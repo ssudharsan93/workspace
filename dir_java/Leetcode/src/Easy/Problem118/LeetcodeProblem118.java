0466eaa0f2fa978a5833c3583266182b3da84c59
ssudharsan93 <sudharsan.ssv@gmail.com>
2024-11-11 13:10:04 -0800

    Solution to Pascal's Triangle ( Problem 118 )

---
+++ dir_java/Leetcode/src/Easy/Problem118/LeetcodeProblem118.java
@@ -0,0 +1,64 @@
+package src.Easy.Problem118;
+
+import java.util.*;
+import java.math.BigInteger;
+import src.Easy.common.ListUtils;
+import src.Easy.common.MathUtils;
+
+class Solution {
+
+        public List<List<Integer>> generate(int numRows) {
+            List<List<Integer>> solution = new ArrayList<>();
+            List<Integer> currentRow = new ArrayList<Integer>();
+
+            if ( numRows <= 0 ){
+                solution.add(currentRow);
+                return solution;
+            }
+
+            for ( int rowIdx = 0; rowIdx < numRows; rowIdx++ ){
+                for ( int colIdx = 0; colIdx <= rowIdx; colIdx++ ){
+                    int val = MathUtils.nCr(rowIdx, colIdx);
+                    currentRow.add(val);
+                }
+                solution.add(currentRow);
+                currentRow = new ArrayList<Integer>();
+            }
+
+            return solution;
+        }
+
+        public void printListofLists(List<List<Integer>> input){
+            System.out.println("[");
+            for ( int listIdx = 0; listIdx < input.size(); listIdx++ ){
+                ListUtils.printList(input.get(listIdx));
+                System.out.println("");
+            }
+            System.out.println("]");
+        }
+
+        public static void main(String[] args){ 
+        Solution sol = new Solution();
+
+        //tc1
+        // int numRows = 5;
+        // List<List<Integer>> result = sol.generate(numRows);
+        // sol.printListofLists(result);
+
+        //tc2
+        // int numRows = 3;
+        // List<List<Integer>> result = sol.generate(numRows);
+        // sol.printListofLists(result);
+
+        //tc3
+        // int numRows = 7;
+        // List<List<Integer>> result = sol.generate(numRows);
+        // sol.printListofLists(result);
+
+        //tc4
+        int numRows = 14;
+        List<List<Integer>> result = sol.generate(numRows);
+        sol.printListofLists(result);
+
+    }
+}
---
+++ dir_java/Leetcode/src/Easy/common/MathUtils.java
@@ -0,0 +1,20 @@
+package src.Easy.common;
+
+import java.util.*;
+import java.math.*;
+
+public class MathUtils {
+    public static int nCr(int n, int k){
+        return MathUtils.factorial(n).divide(MathUtils.factorial(n - k).multiply(MathUtils.factorial(k))).intValue();
+    }
+
+    public static BigInteger factorial(int num){
+        BigInteger result = BigInteger.valueOf((long) 1);
+        while ( num > 0 ){
+            result = result.multiply(BigInteger.valueOf(((long) num)));
+            num = num - 1;
+        }
+
+        return result;
+    }
+}
