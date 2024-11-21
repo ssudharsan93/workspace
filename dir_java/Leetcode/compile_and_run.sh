86064298abed3d191a9eff2170443f982e719dd4
ssudharsan93 <sudharsan.ssv@gmail.com>
2024-11-20 20:16:06 -0800

    new shell script for compiling building and running java code

--- dir_java/Leetcode/compile_and_run.sh
+++ dir_java/Leetcode/compile_and_run.sh
@@ -1,11 +1,26 @@
 #! /usr/bin/sh
-prob_num=$1
+difficulty=$1
+prob_num=$2
 
-cmd1="javac -d build src/Easy/Problem$prob_num/LeetcodeProblem$prob_num.java"
+if [ $# -eq 0 ]; then
+  echo "No arguments provided."
+  exit 1
+
+elif [ $# -eq 1 ]; then
+  echo "Difficulty Missing."
+  exit 1
+
+else
+  echo "Argument(s) provided:" $difficulty $prob_num
+
+fi
+
+cmd1="javac -d build src/${difficulty}/Problem$prob_num/LeetcodeProblem$prob_num.java"
 cmd2="jar cfm bin/LeetcodeProblem$prob_num.jar manifest.txt -C build/ ."
 cmd3="java -jar bin/LeetcodeProblem$prob_num.jar"
 
-sed -i -E "s/[0-9]+/$prob_num/g" manifest.txt
+echo "Main-Class: src.${difficulty}.Problem$prob_num.Solution" > manifest.txt
+
 
 $cmd1
 $cmd2
