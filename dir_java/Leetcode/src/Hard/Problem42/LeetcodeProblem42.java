44121fa03b0f24b8916aa5bf3f8101e45c996dc4
ssudharsan93 <sudharsan.ssv@gmail.com>
2024-11-28 12:16:46 -0800

    Added an explanation for the algorithm in Problem 42 ( Trapping Rain Water )

--- dir_java/Leetcode/src/Hard/Problem42/LeetcodeProblem42.java
+++ dir_java/Leetcode/src/Hard/Problem42/LeetcodeProblem42.java
@@ -1,8 +1,44 @@
 package src.Hard.Problem42;
 
-#TODO
+//TODO
 //Need to write a description of why this algorithm works.
 // Also need to enumerate the key steps of this algorithm.
+
+//https://leetcode.com/problems/trapping-rain-water/description/
+
+// In order to solve this problem we need to determine at each position
+// how much water can feasibly be stored.
+
+// We do this by determining the maximum height seen by a particular index
+// from the left and from the right. This is because we need to know if there
+// this current index is the highest column seen from both left and right or
+// the minimum of the highest columns seen from the left and the right. In either
+// case the amount of water that would be stored would be 0.
+
+// Rain can only collect between two columns and rises to the height of the 
+// smaller of the two columns.
+
+// In order to accomplish this we first need to determine the maximum height
+// seen from the right and from the left for each index, up until that index.
+// i.e. left[i] and right[i] are the maximum height seen from the left and
+// from the right respectively at index i coming from those respective directions.
+// left[i] is the maximum height seen from 0 to i and right[i] is the maximum
+// height seen from i to the end of the array.
+
+// The algorithm is as follows:
+// 1. Initialize left[0] = height[0] and right[length-1] = height[length-1] ( These are 
+// the maximum heights seen from the left and from the right respectively at index 0 i.e.
+// the very first column and the very last column. )
+// 2. For each index i, determine the maximum height seen from the left and from the right
+// and store them in left[i] and right[i] respectively. These are two separate arrays and are
+// populated in two separate forloops.
+// 3. For each index i, determine the amount of water that can be collected at index i based on 
+// the maximum height seen from the left and from the right. The amount of water that can be
+// collected is the minimum of the two heights minus the height at index i. This is because
+// the amount of water that can be collected at that specific point is the height of the column
+// at that point which forms the base of the water up till the height of the lower of the two columns
+// seen from the left and from the right at index i.
+// 4. Return the total amount of water that can be collected.
 
 class Solution {
     public int trap(int[] height) {
@@ -12,18 +48,17 @@ class Solution {
         right[height.length-1] = height[height.length-1];
 
         for (int i = 1; i < height.length; i++){
-            left[i] = Math.max(left[i-1], height[i-1]);
+            left[i] = Math.max(left[i-1], height[i]);
         }
         for (int i = height.length-2; i >= 0; i--){
-            right[i] = Math.max(right[i+1], height[i+1]);
+            right[i] = Math.max(right[i+1], height[i]);
         }
 
         int totalWater = 0;
         for (int i = 0; i < height.length; i++){
             int currTrappedWatter = Math.min(left[i], right[i]) - height[i];
-            if ( currTrappedWatter >= 0 ){
-                totalWater += currTrappedWatter;
-            } 
+            totalWater += currTrappedWatter;
+
         }
         return totalWater;
     }
